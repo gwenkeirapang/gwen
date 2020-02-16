aa7d26814264bf11dac6ed9b80e8c4f8a1101232
Gwen Pang <gwenkeirapang@gmail.com>
2020-02-17 01:31:57 +0800

    commit

---
+++ canvas.js
@@ -0,0 +1,27 @@
+window.addEventListener("load", () => {
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    let painting = false;
+
+    function startPosition() {painting = true}
+
+    function finishedPosition() {painting = false}
+
+    function draw(e){
+        if (!painting) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke()
+    }
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw)
+})
+
